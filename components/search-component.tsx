03258e1fd047480ae48516dcfa6a6aefb3fb6357
Delfim Celestino <delfimcelestinoamissepastola@gmail.com>
2024-08-30 16:39:44 +0200

    feat: add search input and ensure that are working fine

--- components/search-component.tsx
+++ components/search-component.tsx
@@ -1,14 +1,30 @@
+"use client"
+
 import { Input } from "@/components/ui/input"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
 
 const SearchComponent = () => {
+  const [search, setSeach] = useState("")
+
+  const router = useRouter()
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    router.push(`/search/${encodeURIComponent(search)}`)
+  }
+
   return (
-    <div className="my-6 grid gap-2">
+    <form onSubmit={onSubmit} className="my-6 grid gap-2">
       <h1>Quais itens você tem em mente para trocar hoje?</h1>
       <Input
+        required
+        value={search}
+        onChange={(e) => setSeach(e.target.value)}
         className="bg-secondary"
         placeholder="O que deseja trocar, pesquise aqui..."
       />
-    </div>
+    </form>
   )
 }
 
