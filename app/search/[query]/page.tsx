ffa34207d9783d5ea24db6e3929a8ac876d1feb9
Delfim Celestino <delfimcelestinoamissepastola@gmail.com>
2024-09-04 16:24:42 +0200

    fix: fixing search page

--- app/search/[query]/page.tsx
+++ app/search/[query]/page.tsx
@@ -3,20 +3,40 @@ import React from "react"
 import CardProduct from "@/components/card-product"
 import SearchComponent from "@/components/search-component"
 import QuickSearchComponent from "@/components/quick-search-component"
+import { db } from "@/lib/prisma"
 
-const Home = ({ params }: { params: { query: string } }) => {
+const Search = async ({ params }: { params: { query: string } }) => {
   const query = decodeURIComponent(params.query)
 
-  const results = availableItems.filter((item) =>
-    item.productName.toLowerCase().includes(query.toLowerCase()),
-  )
+  const results = await db.products.findMany({
+    where: {
+      OR: [
+        {
+          name: {
+            contains: query,
+          },
+        },
+        {
+          content: {
+            contains: query,
+          },
+        },
+      ],
+    },
+    include: {
+      PorductImages: true, // Corrigido para refletir o nome correto do relacionamento
+      user: true,
+    },
+  })
 
   if (results.length === 0) {
     return (
       <div className="wrapper">
         <SearchComponent />
         <QuickSearchComponent />
-        <p>Nenhum resultado encontrado</p>
+        <p className="my-4 text-sm text-gray-400">
+          Nenhum resultado encontrado...
+        </p>
       </div>
     )
   }
@@ -35,4 +55,4 @@ const Home = ({ params }: { params: { query: string } }) => {
   )
 }
 
-export default Home
+export default Search
