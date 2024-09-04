8069afff8a32f67095fabd20a00e2055f936ea5d
Delfim Celestino <delfimcelestinoamissepastola@gmail.com>
2024-09-04 16:35:49 +0200

    fix: fixing pages rendered

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/", // Rota principal
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "no-store, max-age=0, must-revalidate",
-          },
-        ],
-      },
-    ]
-  },
   httpAgentOptions: {
     keepAlive: true,
   },
